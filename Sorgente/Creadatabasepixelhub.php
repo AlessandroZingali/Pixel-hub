3e04038bfc056715f2a6ef52758f5f2f390317a9
AlessandroZingali <zingali.1899112@studenti.uniroma1.it>
2025-12-11 12:21:48 +0100

    fix database mysql

--- Sorgente/Creadatabasepixelhub.php
+++ Sorgente/Creadatabasepixelhub.php
@@ -5,64 +5,77 @@ $db_name = "Database_Pixel_Hub";
 $table_users = "Tabella_Utenti";
 
 
-$mysqliConnection = new mysqli("localhost", "Alessandro", "belandi", $db_name);
+$mysqliConnection = new mysqli("localhost", "Alessandro", "belandi");
 
 
-if (mysqli_connect_errno($mysqliConnection)) {
+if (mysqli_connect_errno()){
+
     printf("problemi di connessione : %s\n", mysqli_connect_error());
 }
+else{
 
-$queryCreazioneDatabase = "CREATE DATABASE $db_name";
+    printf("Connessione avvenuta con successo ...\n");
+    $queryCreazioneDatabase = "CREATE DATABASE $db_name";
 
-if ($resultQ = mysqli_query($mysqliConnection, $queryCreazioneDatabase)) {
-	printf("Database creato ...\n");
-
-    $sqlQuery = "CREATE TABLE $table_users ( 
-    ID  INT  AUTO_INCREMENT, 
-    Email  VARCHAR  (100), 
-    Password  VARCHAR  (100), 
-    Username  VARCHAR  (50), 
-    Grado  INT, 
-    Pixels  INT, 
-    Saldo_attuale  FLOAT, 
-    Data_di_Nascita  VARCHAR(50), 
-    Nome  VARCHAR  (50), 
-    Cognome VARCHAR (50), 
-    Tipologia_utente INT,  
-    PIVA  VARCHAR (12), 
-    PRIMARY  KEY  (ID,  E-mail), 
-    UNIQUE  KEY  Email_UNIQUE  (E-mail), 
-    UNIQUE  KEY  ID_UNIQUE  (ID)
-    ); 
-    ";
-    echo "<P>$sqlQuery</P>";
-    if ($resultQ = mysqli_query($mysqliConnection, $sqlQuery)){
-
-        printf("Ho creato la tabella Utenti ...\n");
-
-        $sqlQuery = "INSERT INTO $table_users 
-        (Email, Password, Username, Grado, Pixels, Saldo_attuale, Data_di_Nascita, Nome, Cognome, Tipologia_utente, PIVA) 
-        VALUES 
-        (\"marcorossi@gmail.com\", \"marcorossi123\", \"marcorossi\", 1, 1000, 50.0, \"15/04/1990\", \"Marco\", \"Rossi\", 1, \"\"),
-        (\"gabibbo@gmail.com\", \"gabibbo123\", \"gabibbo\", 2, 5000, 200.0, \"20/06/1985\", \"Gabriele\", \"Bianchi\", 2, \"12345678901\");
+    if ($resultQ = mysqli_query($mysqliConnection, $queryCreazioneDatabase)) {
+        
+        printf("Database creato ...\n");
+        $sqlQuery = "CREATE TABLE $table_users ( 
+        ID  INT  AUTO_INCREMENT, 
+        Email  VARCHAR  (100), 
+        Password  VARCHAR  (100), 
+        Username  VARCHAR  (50), 
+        Grado  INT, 
+        Pixels  INT, 
+        Saldo_attuale  FLOAT, 
+        Data_di_Nascita  VARCHAR(50), 
+        Nome  VARCHAR  (50), 
+        Cognome VARCHAR (50), 
+        Tipologia_utente INT,  
+        PIVA  VARCHAR (12), 
+        PRIMARY  KEY  (ID, Email),
+        UNIQUE  KEY  Email_UNIQUE  (Email) 
+        ); 
         ";
+        $mysqliConnection->close();
 
-        if ($resultQ=mysqli_query($mysqliConnection, $sqlQuery)){
-            printf("Installazione DB effettuata! :-)");
+        $mysqliConnection = new mysqli("localhost", "Alessandro", "belandi", $db_name);
+
+        if ($resultQ = mysqli_query($mysqliConnection, $sqlQuery)){
+
+            printf("Ho creato la tabella Utenti ...\n");
+
+            $sqlQuery = "INSERT INTO $table_users 
+            (Email, Password, Username, Grado, Pixels, Saldo_attuale, Data_di_Nascita, Nome, Cognome, Tipologia_utente, PIVA) 
+            VALUES 
+            (\"marcorossi@gmail.com\", \"marcorossi123\", \"marcorossi\", 1, 1000, 50.0, \"15/04/1990\", \"Marco\", \"Rossi\", 1, \"\"),
+            (\"gabibbo@gmail.com\", \"gabibbo123\", \"gabibbo\", 2, 5000, 200.0, \"20/06/1985\", \"Gabriele\", \"Bianchi\", 2, \"12345678901\");
+            ";
+
+            if ($resultQ=mysqli_query($mysqliConnection, $sqlQuery)){
+                printf("Installazione DB effettuata! :-)");
+            }
+            else{
+                printf("Errore popolamento DB.");
+            }
         }
-        else{
-            printf("Errore popolamento DB.");
+        
+        else {
+            printf("Ho avuto un problema per creare la tabella utenti.\n");
         }
     }
-    
     else {
-        printf("Ho avuto un problema per creare la tabella utenti.\n");
+        printf("Ho avuto un problema per creare il database.\n");
     }
-}
-else {
-	printf("Errore creazione Database.\n");
+
+    
 }
 
+if($mysqliConnection){
+    $mysqliConnection->close();
+}
+
+?>
 
 
  
---
+++ Sorgente/elimnadatabase.php
@@ -0,0 +1,35 @@
+<html>
+   <head><title>Elimino DATABASE </title></head>
+   <body>
+      <?php
+
+		$mysqliConnection = new mysqli("localhost", "Alessandro", "belandi");
+         
+         if($resultQ = $mysqli_connect_errno($mysqliConnection)){ {
+       
+
+            printf('connessione avvenuta con successo.<br />');
+            $query="DROP DATABASE Database_Pixel_Hub";
+            $resulQ=mysqli_query($mysqliConnection, $query);
+
+            if ($resulQ) {
+                printf("Database eliminato.<br />");
+            }
+            else {
+                printf("Database non eliminato<br />");
+            }
+
+            $mysqliConnection->close();
+            }
+
+         
+			
+		}
+        else {
+            printf("connessione fallita:<br />");
+        }     
+        ;
+		
+      ?>
+   </body>
+</html>
