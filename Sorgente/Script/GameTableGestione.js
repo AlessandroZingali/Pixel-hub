d1caf1756bca0180cd5ebfa207ed4c2d702b705a
AlessandroZingali <zingali.1899112@studenti.uniroma1.it>
2025-12-13 11:57:09 +0100

    Co-authored-by: Rick88999 <Rick88999@users.noreply.github.com>

--- Sorgente/Script/GameTableGestione.js
+++ Sorgente/Script/GameTableGestione.js
@@ -1,40 +1,61 @@
+var cellCount = 1;
+window.addEventListener("load", scaleTableOnLoad);
+window.addEventListener("resize", scaleTableOnResize);
 
-/*function scaleTable() {
-    //alert("cazzo");
-    var width = window.innerWidth;
-    var table = document.getElementById("GameTable");
-    var gameRow = table.row[0];
-    alert("${width}");
-    if(width<=700){
-        gameRow.deleteCell(gameRow.cells.length - 1);
-    }
-    
-}*/
+function scaleTableOnResize(){
 
-
-window.addEventListener("resize", scaleTable);
-
-function scaleTable(){
     var width = window.innerWidth;
     var table = document.getElementById("GameTable");
     var gameRow = table.rows[1];
-    var cellCount = gameRow.cells.length;
-    var i;
 
-    alert("larghezza schermo "+`${width}`);
+    //alert("larghezza schermo " +`${width}`);
 
-    if(width<= 1200 && cellCount > 4){
-        //gameRow.deleteCell(gameRow.cells.length - 1);
-        gameRow.cells[cellCount - 1].style.display = "none";
-        cellCount=
-        //alert("lunghezza celle "+`${gameRow.cells.length}`+" and Contenuto cella "+`${gameRow.cells[gameRow.cells.length - 1].innerText}`); 
-        //alert("rimosso");
-        //alert(`${gameRow.cells.length}`);
+    if(width<= 1200 && cellCount == 1){
+        gameRow.cells[gameRow.cells.length - cellCount].style.display = "none";
+        cellCount++;
 
     }
-        else if(width<= 1000 && cellCount > 3){
-        //gameRow.deleteCell(gameRow.cells.length - 1);
-        gameRow.cells[cellCount - 1].style.display = "none";
-    
-    } else 
+     else if(width<= 1000 && cellCount == 2){
+        gameRow.cells[gameRow.cells.length - cellCount].style.display = "none";
+        cellCount++;
+    } 
+    else if(width<=1000 && cellCount == 1){
+        for(let i=0; i<2; i++){
+            gameRow.cells[gameRow.cells.length - cellCount].style.display = "none";
+            cellCount++;
+        }
+
+    }
+    else if (width>1000 && cellCount == 3){
+        gameRow.cells[gameRow.cells.length - (cellCount -1)].style.display = "table-cell";
+        cellCount--;
+    }
+    else if (width>1200 && cellCount == 2){
+        gameRow.cells[gameRow.cells.length - (cellCount -1)].style.display = "table-cell";
+        cellCount--;
+    }
+    else if (width>1200 && cellCount == 3){
+        for(let i=0; i<2; i++){
+            gameRow.cells[gameRow.cells.length - (cellCount -1)].style.display = "table-cell";
+            cellCount--;
+        }
+    }
+}
+
+function scaleTableOnLoad(){
+    var width = window.innerWidth;
+    var table = document.getElementById("GameTable");
+    var gameRow = table.rows[1];    
+
+    if(width<=1000 && cellCount == 1){
+        for(let i=0; i<2; i++){
+            gameRow.cells[gameRow.cells.length - cellCount].style.display = "none";
+            cellCount++;
+        }
+    }
+    else if(width<= 1200 && cellCount == 1){
+        gameRow.cells[gameRow.cells.length - cellCount].style.display = "none";
+        cellCount++;
+    }
+
 }
