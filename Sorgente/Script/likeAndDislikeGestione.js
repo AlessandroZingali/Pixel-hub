ef12e6e79eab9f7c3c80cb3e7ce7fd3221c4a762
Ryckie45 <131523932+Rick88999@users.noreply.github.com>
2025-12-28 19:53:13 +0100

    Aggiornamento likeAndDislikeGestione.js
    correzione poszione send e eliminazione dicharazione per let --> var

--- Sorgente/Script/likeAndDislikeGestione.js
+++ Sorgente/Script/likeAndDislikeGestione.js
@@ -4,6 +4,7 @@ function LikeGestione(idUtente, idCommento, idGioco, tipo) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", "LikeCommenti.xml", true);
 
+
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
             var response = xmlHttp.responseXML;
@@ -24,6 +25,7 @@ function LikeGestione(idUtente, idCommento, idGioco, tipo) {
                 } 
             }
         }
+xmlHttp.send(null);
             
        
 
@@ -33,24 +35,24 @@ function LikeGestione(idUtente, idCommento, idGioco, tipo) {
         var xmlHttp2 = new XMLHttpRequest();
         xmlHttp2.open("POST", "aggiornamentoLikeCommenti.php", true);
         xmlHttp2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlHttp2.send("idUtente=" + idUtente + "&idCommento=" + idCommento + "&idGioco=" + idGioco + "&tipo="+ tipo + "&route=0");
+        
         xmlHttp2.onreadystatechange = function() {
             if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
                 if(tipo === "like") {
                     
-                    let likeText = document.getElementById("likeButtonText"+idCommento);
+                    var likeText = document.getElementById("likeButtonText"+idCommento);
                     likeText.innerHTML = parseInt(likeText.innerHTML) + 1;
                     document.getElementById("likeButton"+idCommento).style.backgroundColor = "green";
                 }
                 else if(tipo === "dislike") {
-                    let dislikeText = document.getElementById("dislikeButtonText"+idCommento);
+                    var dislikeText = document.getElementById("dislikeButtonText"+idCommento);
                     dislikeText.innerHTML = parseInt(dislikeText.innerHTML) + 1;
                     document.getElementById("dislikeButton"+idCommento).style.backgroundColor = "red";
                 }
 
             }
         }
-
+xmlHttp2.send("idUtente=" + idUtente + "&idCommento=" + idCommento + "&idGioco=" + idGioco + "&tipo="+ tipo + "&route=0");
     }
     else if (found && ((tipo === "like" && flagLike === "0") || (tipo === "dislike" && flagDislike === "0"))) {
         // ha già messo like e vuole cambiare in dislike 
@@ -58,10 +60,10 @@ function LikeGestione(idUtente, idCommento, idGioco, tipo) {
             var xmlHttp3 = new XMLHttpRequest();
         xmlHttp3.open("POST", "aggiornamentoLikeCommenti.php", true);
         xmlHttp3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlHttp3.send("idUtente=" + idUtente + "&idCommento=" + idCommento + "&idGioco=" + idGioco + "&tipo="+ tipo+ "&route=1");
+        
         xmlHttp3.onreadystatechange = function() {
             if (xmlHttp3.readyState === 4 && xmlHttp3.status === 200) {
-                let likeText = document.getElementById("likeButtonText"+idCommento);
+                var likeText = document.getElementById("likeButtonText"+idCommento);
                 likeText.innerHTML = parseInt(likeText.innerHTML) - 1;
                 document.getElementById("likeButton"+idCommento).style.backgroundColor = "";
                 let dislikeText = document.getElementById("dislikeButtonText"+idCommento);
@@ -71,24 +73,25 @@ function LikeGestione(idUtente, idCommento, idGioco, tipo) {
                 alert("Cambio effettuato con successo! like -> dislike");
             }
         }
-            
+     xmlHttp3.send("idUtente=" + idUtente + "&idCommento=" + idCommento + "&idGioco=" + idGioco + "&tipo="+ tipo+ "&route=1");       
     }
     else if(tipo === "dislike") {
         // ha già messo dislike e vuole cambiare in like
         var xmlHttp4 = new XMLHttpRequest();
         xmlHttp4.open("POST", "aggiornamentoLikeCommenti.php", true);
         xmlHttp4.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlHttp4.send("idUtente=" + idUtente + "&idCommento=" + idCommento + "&idGioco=" + idGioco + "&tipo="+ tipo + "&route=2");
+        
         xmlHttp4.onreadystatechange = function() {
             if (xmlHttp4.readyState === 4 && xmlHttp4.status === 200) {
 
-                let dislikeText = document.getElementById("dislikeButtonText"+idCommento);
+                var dislikeText = document.getElementById("dislikeButtonText"+idCommento);
                 dislikeText.innerHTML = parseInt(dislikeText.innerHTML) - 1;    
                 document.getElementById("dislikeButton"+idCommento).style.backgroundColor = "";
-                let likeText = document.getElementById("likeButtonText"+idCommento);
+                var likeText = document.getElementById("likeButtonText"+idCommento);
                 alert("Cambio effettuato con successo! dislike -> like");
             }
         }
+xmlHttp4.send("idUtente=" + idUtente + "&idCommento=" + idCommento + "&idGioco=" + idGioco + "&tipo="+ tipo + "&route=2");
     }
     else {
         alert("Hai già messo like o dislike a questo commento!");
@@ -96,5 +99,5 @@ function LikeGestione(idUtente, idCommento, idGioco, tipo) {
 }
 }
 
-    xmlHttp.send(null);
+    
 }
