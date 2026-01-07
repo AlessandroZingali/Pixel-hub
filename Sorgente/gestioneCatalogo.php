c9fe6a8a8cc7383a893f12286328851912bc96f4
AlessandroZingali <zingali.1899112@studenti.uniroma1.it>
2026-01-07 16:32:55 +0100

    Rimosso codice obsoleto per la gestione del catalogo e delle dimensioni della tabella

--- Sorgente/gestioneCatalogo.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(isset($_POST['visibili'])) $_SESSION['NumCol']=$_POST['visibili'];
-else if (isset($_POST['visibili'])) setcookie('NumCol', $_POST['visibili']);
-echo "Risposta: ".$_POST['visibili']." && Sessione: ".$_SESSION['NumCol'];
-
-?>
